bdddcde65ef0126025c68701861f0fa47aeefe50
unknown <Sanjay_Shivanna@MYSHEZ94928L.ad.infosys.com>
2016-09-06 23:40:45 +0200

    Initial commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by Sanjay_Shivanna on 9/6/2016.
+ */
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
